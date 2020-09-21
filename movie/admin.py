40050d4c5fb2e4f7cc007b7187ae2c973b5e6d2b
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2020-09-21 17:35:11 +0500

    solve error

--- movie/admin.py
+++ movie/admin.py
@@ -17,8 +17,8 @@ class MovieShotsInline(admin.TabularInline):
     extra = 1
     readonly_fields = ("get_image",)
     def get_image(self, obj):
-        return mark_safe(f'<img src={obj.image.url} width="50" height="60">')
-    get_image.short_description = 'Изображение'  
+        return mark_safe(f'<img src={obj.image.url} width="100" height="120">')
+    get_image.short_description = 'Изображение'
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
@@ -29,12 +29,13 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft", )
+    readonly_fields = ("get_image",)
     fieldsets = (
     (None, {
     "fields": (("title", "tagline"), )
     }),
     (None, {
-    "fields": (("description", "poster"), )
+    "fields": ("description", "poster", "get_image")
     }),
     (None, {
     "fields": (("year", "world_premiere", "country"), )
@@ -49,7 +50,10 @@ class MovieAdmin(admin.ModelAdmin):
     ('Options', {
     "fields": (("url", "draft"), )
     }),
-    )
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="110">')
+
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
