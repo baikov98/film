d6c8c43d4a65a18a7ba9f158704fe2d5d80ea242
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2020-09-22 20:22:27 +0500

    sdsd

--- movie/views.py
+++ movie/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 # Create your views here.
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
 
 class MoviesView(ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    #template_name = 'movies/movies.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 class AddReview(View):
     def post(self, request, pk):
